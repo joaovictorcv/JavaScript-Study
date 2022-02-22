2a1d479b7524aa323c7e6d719143fc2f8c347412
joaobout007@gmail.com <joaovictorcavalcante721@gmail.com>
2022-02-21 21:58:01 -0300

    Fist Commit

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+// /*forma de mostrar (output)*/
+
+// document.getElementById("title").innerHTML ="Opa, tudo bem?"
+// document.write('Algum texto')
+// window.alert("ALERTA ALERTA")
+// console.log("A mensagem que eu inserir aqui aparecera no console")
+
+
+var age = 18;
+
+var x = 10;
+var y = 15;
+
+var result = x + y;
+
+
+var name = "João Victor";
+var lastName = 'Cavalcante';
+
+var nameComplet = name + " " + lastName; //Contenou as aspas vazia como o espaço entre o nome e sobrenome//
+alert (nameComplet);
+
+
