088cff4bb94a1477167a52c9b888ca2eb6af2f21
joaobout007@gmail.com <joaovictorcavalcante721@gmail.com>
2022-02-22 14:30:38 -0300

    Studying the conditional

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@
 // window.alert("ALERTA ALERTA")
 // console.log("A mensagem que eu inserir aqui aparecera no console")
 
-
+//variaveis//
 var age = 18;
 
 var x = 10;
@@ -14,10 +14,44 @@ var y = 15;
 var result = x + y;
 
 
-var name = "João Victor";
+var firstName = "João Victor";
 var lastName = 'Cavalcante';
 
 var nameComplet = name + " " + lastName; //Contenou as aspas vazia como o espaço entre o nome e sobrenome//
 alert (nameComplet);
+
+//Condicionais//
+
+var hora = 12;
+
+if ( hora < 12 ) {
+    console.log("Good Morning");
+} else if ( hora < 18){
+    console.log("Good Afternoon")
+} else if ( hora <= 23){
+    console.log("Good Night")
+}
+
+if ( hora >= 12 && hora < 18 ){
+    console.log("Good Afternoon2");
+}
+
+if ( hora == 12 || hora == 18 ){
+    console.log("Você está na hora do rush")
+}
+
+if( lastName == 'Cavalcante'){
+    console.log("you are from the cavalcante family")
+}
+
+if( firstName == 'João Victor' && lastName == 'Cavalcante' ){
+    console.log("You is the correct");
+}
+
+if (firstName == 'João Victor'){
+    console.log("Yes BRO =)")
+}else{
+    console.log("No BRO =*(")
+}
 
 
